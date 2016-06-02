96b618427edfc6c42eb919dac466e33143df7e67
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-06-02 17:39:57 +0000

    field validation (backend)

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,15 @@ class Product
   field :images, type: String
   field :price, type: String
   field :country, type: String
-  field :display, type: Integer
-  field :views, type: Integer
-  field :click, type: Integer
+  field :display, type: Integer, default: 0
+  field :views, type: Integer, default: 0
+  field :click, type: Integer, default: 0
+
+  validates :name, presense: true
+  validates :description, presense: true
+  validates :url, presense: true, :url => true
+  validates :domain, presense: true, :url => true
+  validates :images, :url => true
+  validates :price, :presense: true
+  validates :country, :presense: true, inclusion: {in: %w(USA Canada Test)}
 end
