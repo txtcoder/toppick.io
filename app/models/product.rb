867b5e0b6ea399e3cfde91d93951926843c935ed
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-06-02 17:55:29 +0000

    4

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,11 +11,11 @@ class Product
   field :views, type: Integer, default: 0
   field :click, type: Integer, default: 0
 
-  validates :name, presense: true
-  validates :description, presense: true
-  validates :url, presense: true, :url => true
-  validates :domain, presense: true, :url => true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :url, presence: true, :url => true
+  validates :domain, presence: true, :url => true
   validates :images, :url => true
-  validates :price, :presense: true
-  #validates :country, :presense: true, inclusion: { in: %w(USA Canada Test), message: "%{value} is not a supported country" }
+  validates :price, :presence: true
+  validates :country, :presence: true, inclusion: { in: %w(USA Canada Test), message: "%{value} is not a supported country" }
 end
