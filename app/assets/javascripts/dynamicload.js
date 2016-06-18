d76e1ed094318f6a44ea41de71040342d192136b
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-06-18 00:01:29 +0000

    get active buttons working

--- app/assets/javascripts/dynamicload.js
+++ app/assets/javascripts/dynamicload.js
@@ -1,4 +1,13 @@
+clear_active = function () {
+    $(".view_all").removeClass("active");
+    $(".top_viewed").removeClass("active");
+    $(".editor_pick").removeClass("active");
+    $(".new_old").removeClass("active");
+};
+
 get_hot = function() {
+    clear_active();
+    $(".view_all").addClass("active");
     $.ajax({
         type: 'GET',
         url: '/?sort=hot&partial=hot',
@@ -9,6 +18,8 @@ get_hot = function() {
     });
 };
 get_views = function() {
+    clear_active();
+    $(".top_viewed").addClass("active");
     $.ajax({
         type: 'GET',
         url: '/?sort=views&partial=views',
@@ -19,6 +30,8 @@ get_views = function() {
     });
 };
 get_editor = function() {
+    clear_active();
+    $(".editor_pick").addClass("active");
     $.ajax({
         type: 'GET',
         url: '/?sort=editor&partial=editor',
@@ -29,6 +42,8 @@ get_editor = function() {
     });
 };
 get_new = function() {
+    clear_active();
+    $(".new_old").addClass("active");
     $.ajax({
         type: 'GET',
         url: '/?sort=new&partial=new',
