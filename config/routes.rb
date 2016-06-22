fb933bf09c26465f0b1b2f31cb7ba9025980061a
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-06-22 03:11:50 +0000

    login admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup' => 'admins#new'
+  resources :admins
   get 'login' =>'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
