9a8d2ddd7113a8332d912fa500effc63113fee39
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-08-11 20:14:55 +0000

    admin ui change

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,12 @@
 class Product
   include Mongoid::Document
   include Mongoid::Timestamps
-  before_validation :update_affiliate_link
   before_save :update_ratio
 
   field :name, type: String
   field :one_liner, type: String
   field :description, type: String
-  field :url, type: String
-  field :domain, type: String
   field :images, type: String
-  field :price, type: String
-  field :country, type: String
   field :specs, type: String
   field :editor_pick, type: Boolean, default: false
   field :approved, type: Boolean, default: false
@@ -25,12 +20,7 @@ class Product
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :url, presence: true, :url => true
-  validates :domain, presence: true
-  validates :price, presence: true
-  validates :country, presence: true, inclusion: { in: %w(USA Canada Test), message: "%{value} is not a supported country" }
 
-  scope :test, -> {where(country: "Test")}
   scope :USA, -> { where('links.country' => 'USA')}
   scope :Canada, ->{where('links.country'=> 'Canada')}
   scope :sort_by_new, -> {order_by(:created_at => :desc)}
@@ -38,27 +28,6 @@ class Product
   scope :most_viewed, -> {order_by(:views => :desc)}
   scope :editor_pick, ->{where(editor_pick: true)}
   default_scope -> {where(approved: true)}
-
-  def update_affiliate_link
-    domain= URI.parse(url).host
-    self.domain=domain
-    self.url=url.gsub(/\?.*/, "")
-    #res = Affiliate.find_by( domain: domain)
-    #if res
-    #    referral = res.referral
-    #    if url.include? referral
-    #        self.url=url
-    #    elsif url.include? "?" and url[-1]!="/"
-    #        self.url = url+"&"+referral
-    #    elsif url.include? "?"
-    #        self.url= url[0..-1]+"&"+referral
-    #    elsif url[-1]!="/"
-    #        self.url=url+"/?"+referral
-    #    else
-    #        self.url=url+"?"+referral
-    #    end
-    #end    
-  end
 
   def update_ratio
      self.d_to_c = self.click/(self.display+1.0)
