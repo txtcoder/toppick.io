70c58c62bc12d1e4fbf17297a0ffb2daf38a9f8a
txtcoder <tommytong.is.here+txtcoder@gmail.com>
2016-08-12 17:38:29 +0000

    delete image working

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -66,7 +66,7 @@ class ImagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_image
-      @image = Image.find(params[:id])
+      @image = @product.medias.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
